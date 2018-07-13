ce867a671101b6c94ce374c5bd894f05beb7f36c
SAE\bwalker11 <1010579@student.sae.edu.au>
2018-07-13 12:40:24 +1000

    Update audio manager and sciptable audio

---
+++ Assets/Scripts/ScriptableAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Audio")]
+public class ScriptableAudio : ScriptableObject
+{
+    public AudioClip[] Clip;
+    [Range(0f,2f)]
+    public float Pitch;
+    [Range(0f, 100f)]
+    public float Volume;
+    private AudioSource Source;
+    
+
+    public void SetSource(AudioSource Source2)
+    {
+        for (int i = 0; i < Clip.Length; i++)
+        {
+            Source = Source2;
+            Source.clip = Clip[i];
+        }
+    }
+
+    public void Play()
+    {
+        for (int i = 0; i < Clip.Length; i++)
+        {
+            Source.PlayOneShot(Clip[i]);
+        }
+    }
+}
+/*
+ * Scriptable objects created as New Audio
+ * Contains variables for audioclips
+ * Set source of Audio
+ * Play function 
+ */
